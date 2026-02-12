b0cb9c62df704f0d699f8d77c1139227465b4c49
Antonín <av@flwn.dev>
2026-02-12 01:57:21 +0100

    added more properties to gameobjects

--- Latibule/Models/GameObject.cs
+++ Latibule/Models/GameObject.cs
@@ -2,21 +2,30 @@
 
 namespace Latibule.Models;
 
-public class GameObject
+public class GameObject(Game game)
 {
     public Vector3 Position { get; set; } = Vector3.Zero;
+
+    public Vector3 Rotation { get; set; } = Vector3.Zero;
+
     public Vector3 Scale { get; set; } = Vector3.One;
     public Vector2 UVScale { get; set; } = Vector2.One;
 
-    public GameObject(Game game)
-    {
-    }
+    public BoundingBox BoundingBox { get; protected set; }
+
+    public GameObject[] Children { get; set; } = [];
 
     public virtual void Initialize()
+    {
+        LatibuleGame.GameWorld.AddObject(this);
+    }
+
+    public virtual void Update(GameTime gameTime)
     {
     }
 
     public virtual void Draw(GameTime gameTime)
     {
+        // foreach (var child in Children) child.Draw(gameTime);
     }
 }
