cd0b03a74b495fed602c530164c52c60f1c38e43
Antonín <av@flwn.dev>
2026-02-12 01:58:06 +0100

    Added Keyboard and Mouse state + some other props

--- Latibule/GameStates.cs
+++ Latibule/GameStates.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Xna.Framework;
+﻿using Latibule.Models;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Input;
 
 namespace Latibule;
@@ -6,12 +7,18 @@ namespace Latibule;
 public static class GameStates
 {
     // Keyboard and mouse states and mouse handling
+    public static KeyboardState KState { get; set; } = new();
+    public static MouseState MState { get; set; } = new();
     public static KeyboardState PreviousKState { get; set; }
     public static MouseState PreviousMState { get; set; }
-    public static bool MouseLocked { get; set; } = true;
+    public static bool MouseLookLocked { get; set; } = false;
+
+    public static bool HasDeveloperKey { get; set; } = false;
 
     // Game-related properties
+    public static Game Game { get; set; } = null!;
     public static GameTime GameTime { get; private set; } = new();
+    public static IGuiScreen? CurrentGui { get; set; }
 
     public static bool ShowHud { get; set; } = true;
 
