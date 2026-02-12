1576aa2d920f728609f6a61cdaf4aa80987468d6
Antonín <av@flwn.dev>
2026-02-12 01:59:13 +0100

    wip corridor object

---
+++ Latibule/Core/Rendering/Objects/Corridor.cs
@@ -0,0 +1,30 @@
+﻿using Latibule.Models;
+using Microsoft.Xna.Framework;
+
+namespace Latibule.Core.Rendering.Objects;
+
+public class Corridor(Game game) : GameObject(game)
+{
+    private readonly Game _game = game;
+
+    public override void Initialize()
+    {
+        var localScale = new Vector3(2, 0, 2);
+        var uvScale = new Vector2(1, 1);
+
+        Children =
+        [
+            new Plane(_game) { Position = new Vector3(0, 0, 0) + Position, Rotation = new Vector3(0, 0, 0), Scale = localScale, UVScale = uvScale },
+            new Plane(_game) { Position = new Vector3(2, 0, 0) + Position, Rotation = new Vector3(0, 0, 0), Scale = localScale, UVScale = uvScale },
+            new Plane(_game) { Position = new Vector3(-2, 0, 0) + Position, Rotation = new Vector3(0, 0, 0), Scale = localScale, UVScale = uvScale },
+            new Plane(_game) { Position = new Vector3(0, 2, 0) + Position, Rotation = new Vector3(0, 0, 0), Scale = localScale, UVScale = uvScale },
+        ];
+
+        base.Initialize();
+    }
+
+    public override void Update(GameTime gameTime)
+    {
+        base.Update(gameTime);
+    }
+}
