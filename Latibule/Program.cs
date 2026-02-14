5df30ff882ded2a6819ccaefe19c8bbefd5bf001
Antonín <av@flwn.dev>
2026-02-14 09:43:18 +0100

    entry program for opentk

---
+++ Latibule/Program.cs
@@ -0,0 +1,41 @@
+﻿using OpenTK.Mathematics;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Common.Input;
+using OpenTK.Windowing.Desktop;
+using SixLabors.ImageSharp.PixelFormats;
+
+namespace Latibule;
+
+public static class Program
+{
+    private static readonly NativeWindowSettings _windowSettings = new()
+    {
+        ClientSize = new Vector2i(1280, 720),
+        Title = "Latibule but OpenTK",
+        Icon = LoadIcon("Assets/icon.jpg"),
+        APIVersion = new Version(4, 6),
+        Flags = ContextFlags.ForwardCompatible,
+    };
+
+    private static WindowIcon LoadIcon(string path)
+    {
+        using var image = SixLabors.ImageSharp.Image.Load<Rgba32>(path);
+
+        var pixels = new byte[4 * image.Width * image.Height];
+        image.CopyPixelDataTo(pixels);
+
+        return new WindowIcon(
+            new Image(
+                image.Width,
+                image.Height,
+                pixels
+            )
+        );
+    }
+
+    public static void Main()
+    {
+        using var game = new LatibuleGame(_windowSettings);
+        game.Run();
+    }
+}
