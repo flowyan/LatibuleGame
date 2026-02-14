23deed856c81f829e0f9e1a23e4d63573a96479a
Antonín <av@flwn.dev>
2026-02-14 18:04:36 +0100

    corridor model/object updated

---
+++ Latibule/Core/Rendering/Objects/CorridorObject.cs
@@ -0,0 +1,51 @@
+﻿using Latibule.Core.Components;
+using Latibule.Core.ECS;
+using Latibule.Core.Rendering.Models;
+using OpenTK.Mathematics;
+
+namespace Latibule.Core.Rendering.Objects;
+
+public class CorridorObject(Shader shader) : GameObject()
+{
+    public override void OnLoad()
+    {
+        var uvScale = new Vector2(1, 1);
+
+        var shaderComponent = new ShaderComponent(shader)
+        {
+            Texture = new Texture($"{Metadata.ASSETS_ROOT_DIRECTORY}/{Metadata.ASSETS_TEXTURE_PATH}/material/stone.jpg"),
+            UVScale = uvScale,
+        };
+
+        AddChildren([
+            PlaneMaker(CorridorFace.Floor).WithComponent(shaderComponent),
+            PlaneMaker(CorridorFace.Ceiling).WithComponent(shaderComponent),
+            PlaneMaker(CorridorFace.LeftWall).WithComponent(shaderComponent),
+            PlaneMaker(CorridorFace.RightWall).WithComponent(shaderComponent)
+        ]);
+
+        base.OnLoad();
+    }
+
+    private enum CorridorFace
+    {
+        Floor,
+        Ceiling,
+        LeftWall,
+        RightWall,
+    }
+
+    private PlaneObject PlaneMaker(CorridorFace face)
+    {
+        var localScale = new Vector3(2, 0, 2);
+
+        return face switch
+        {
+            CorridorFace.Floor => new PlaneObject() { Transform = { Position = new Vector3(0, 0, 0) + Transform.Position, Rotation = new Vector3(0, 0, 0), Scale = localScale } },
+            CorridorFace.Ceiling => new PlaneObject() { Transform = { Position = new Vector3(0, 4, 0) + Transform.Position, Rotation = new Vector3(180, 0, 0), Scale = localScale } },
+            CorridorFace.LeftWall => new PlaneObject() { Transform = { Position = new Vector3(0, 2, -2) + Transform.Position, Rotation = new Vector3(90, 0, 0), Scale = localScale } },
+            CorridorFace.RightWall => new PlaneObject() { Transform = { Position = new Vector3(0, 2, 2) + Transform.Position, Rotation = new Vector3(270, 0, 0), Scale = localScale } },
+            _ => throw new ArgumentOutOfRangeException(nameof(face), face, null)
+        };
+    }
+}
