f806a6ea964fcfa80f45c3e36ed9cbb5483291cb
Antonín <av@flwn.dev>
2026-02-14 02:09:39 +0100

    cmd to reload/recreate the entire world, for hotloading

---
+++ Latibule/Commands/ReloadWorld.cs
@@ -0,0 +1,22 @@
+﻿using Latibule.Core;
+using Latibule.Core.Rendering;
+using Latibule.Models;
+
+namespace Latibule.Commands;
+
+public class ReloadWorld : ICommand
+{
+    public string Name { get; } = "reloadworld";
+    public List<string> Aliases { get; } = [];
+    public string Usage { get; } = "reloadworld";
+
+    public Task Execute(string[] args)
+    {
+        LatibuleGame.GameWorld.Dispose();
+        LatibuleGame.GameWorld = new World();
+        LatibuleGame.GameWorld = LatibuleGame.CreateWorld(GameStates.Game);
+        LatibuleGame.GameWorld.Initialize();
+        DevConsole.CommandLog("RELOADING WORLD");
+        return Task.CompletedTask;
+    }
+}
