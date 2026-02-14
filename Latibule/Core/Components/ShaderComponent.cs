e2c7f5f3afb77efff9007fedb7b11311175b0db1
Antonín <av@flwn.dev>
2026-02-14 18:02:46 +0100

    refactor: update ShaderComponent to use init properties and improve disposal logic

--- Latibule/Core/Components/ShaderComponent.cs
+++ Latibule/Core/Components/ShaderComponent.cs
@@ -1,19 +1,22 @@
 ﻿using Latibule.Core.ECS;
 using Latibule.Core.Rendering;
+using OpenTK.Mathematics;
+using OpenTK.Windowing.Common;
 using OpenTK.Windowing.Desktop;
 
 namespace Latibule.Core.Components;
 
-public sealed class ShaderComponent(Shader shader) : BaseComponent()
+public class ShaderComponent(Shader shader) : BaseComponent
 {
     public Shader Shader { get; } = shader;
-
-    public Texture? Texture { get; set; }
+    public Texture? Texture { get; init; }
+    public Vector2 UVScale { get; init; } = Vector2.One;
 
     public override void Dispose()
     {
+        base.Dispose();
         Shader.Dispose();
         Texture?.Dispose();
-        base.Dispose();
+        GC.SuppressFinalize(this);
     }
 }
