8d787d13ce67d86dc1a134b0a17e67eac785f6db
Antonín <av@flwn.dev>
2026-02-14 09:47:51 +0100

    utils for converting vector3 and getting direction

---
+++ Latibule/Utilities/Vector3Converter.cs
@@ -0,0 +1,14 @@
+﻿namespace Latibule.Utilities;
+
+public static class Vector3Converter
+{
+    public static OpenTK.Mathematics.Vector3 ToOpenTK(this System.Numerics.Vector3 vector)
+    {
+        return new OpenTK.Mathematics.Vector3(vector.X, vector.Y, vector.Z);
+    }
+
+    public static System.Numerics.Vector3 ToNumerics(this OpenTK.Mathematics.Vector3 vector)
+    {
+        return new System.Numerics.Vector3(vector.X, vector.Y, vector.Z);
+    }
+}
