a11c0fadb329454bb27849e18f55482488fe6aa4
Antonín <av@flwn.dev>
2026-02-14 18:04:23 +0100

    turned planemodel into planeobject/planerenderercomponent

---
+++ Latibule/Core/Rendering/Objects/PlaneObject.cs
@@ -0,0 +1,20 @@
+﻿using Latibule.Core.Components;
+using Latibule.Core.ECS;
+using Latibule.Core.Rendering.Mesh;
+
+namespace Latibule.Core.Rendering.Models;
+
+/// <summary>
+/// A flat plane for ground/floor rendering and collision.
+/// </summary>
+public class PlaneObject() : GameObject
+{
+    public override void OnLoad()
+    {
+        base.OnLoad();
+        WithComponents([
+            new PlaneRendererComponent(),
+            new CollisionComponent()
+        ]);
+    }
+}
