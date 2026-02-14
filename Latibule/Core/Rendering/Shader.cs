7f50a9a344ffceabe7983b0651ec5eab3aff0c45
Antonín <av@flwn.dev>
2026-02-14 09:49:53 +0100

    Shader importer/creater

---
+++ Latibule/Core/Rendering/Shader.cs
@@ -0,0 +1,107 @@
+﻿using OpenTK.Graphics.OpenGL;
+using OpenTK.Mathematics;
+
+namespace Latibule.Core.Rendering;
+
+public sealed class Shader
+{
+    private readonly int Handle;
+
+    public Shader(string vertexPath, string fragmentPath)
+    {
+        string VertexShaderSource = File.ReadAllText(vertexPath);
+
+        string FragmentShaderSource = File.ReadAllText(fragmentPath);
+
+        var VertexShader = GL.CreateShader(ShaderType.VertexShader);
+        GL.ShaderSource(VertexShader, VertexShaderSource);
+
+        var FragmentShader = GL.CreateShader(ShaderType.FragmentShader);
+        GL.ShaderSource(FragmentShader, FragmentShaderSource);
+
+        GL.CompileShader(VertexShader);
+
+        GL.GetShader(VertexShader, ShaderParameter.CompileStatus, out int vertexSuccess);
+        if (vertexSuccess == 0)
+        {
+            string infoLog = GL.GetShaderInfoLog(VertexShader);
+            Console.WriteLine(infoLog);
+        }
+
+        GL.CompileShader(FragmentShader);
+
+        GL.GetShader(FragmentShader, ShaderParameter.CompileStatus, out int fragmentSuccess);
+        if (fragmentSuccess == 0)
+        {
+            string infoLog = GL.GetShaderInfoLog(FragmentShader);
+            Console.WriteLine(infoLog);
+        }
+
+        Handle = GL.CreateProgram();
+
+        GL.AttachShader(Handle, VertexShader);
+        GL.AttachShader(Handle, FragmentShader);
+
+        GL.LinkProgram(Handle);
+
+        GL.GetProgram(Handle, GetProgramParameterName.LinkStatus, out int success);
+        if (success == 0)
+        {
+            string infoLog = GL.GetProgramInfoLog(Handle);
+            Console.WriteLine(infoLog);
+        }
+
+        GL.DetachShader(Handle, VertexShader);
+        GL.DetachShader(Handle, FragmentShader);
+        GL.DeleteShader(FragmentShader);
+        GL.DeleteShader(VertexShader);
+    }
+
+    public int GetAttribLocation(string attribName)
+    {
+        return GL.GetAttribLocation(Handle, attribName);
+    }
+
+    public void SetMatrix4(string name, Matrix4 data)
+    {
+        int location = GL.GetUniformLocation(Handle, name);
+        GL.UniformMatrix4(location, false, ref data);
+    }
+
+    public void SetInt(string name, int value)
+    {
+        int location = GL.GetUniformLocation(Handle, name);
+        GL.Uniform1(location, value);
+    }
+
+    public void Use()
+    {
+        GL.UseProgram(Handle);
+    }
+
+    private bool _disposedValue;
+
+    private void Dispose(bool _)
+    {
+        if (!_disposedValue)
+        {
+            GL.DeleteProgram(Handle);
+            _disposedValue = true;
+        }
+    }
+
+    ~Shader()
+    {
+        if (!_disposedValue)
+        {
+            Console.WriteLine("GPU Resource leak! Did you forget to call Dispose()?");
+        }
+    }
+
+
+    public void Dispose()
+    {
+        Dispose(true);
+        GC.SuppressFinalize(this);
+    }
+}
