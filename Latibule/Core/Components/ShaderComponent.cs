ce0c13e1f87dfbfedb7c2f11383f3b912e2e1151
Antonín <av@flwn.dev>
2026-02-14 09:50:43 +0100

    wip shader component

---
+++ Latibule/Core/Components/ShaderComponent.cs
@@ -0,0 +1,25 @@
+﻿using Latibule.Core.ECS;
+using OpenTK.Windowing.Desktop;
+
+namespace Latibule.Core.Components;
+
+public sealed class ShaderComponent(GameWindow gameWindow) : BaseComponent(gameWindow)
+{
+    // public BasicEffect Effect { get; } = new(gameWindow.GraphicsDevice)
+    // {
+    //     TextureEnabled = true,
+    //     LightingEnabled = false,
+    // };
+
+    // public Texture2D? Texture
+    // {
+    //     get => Effect.Texture;
+    //     set => Effect.Texture = value;
+    // }
+
+    // public override void Dispose()
+    // {
+    //     Effect.Dispose();
+    //     base.Dispose();
+    // }
+}
