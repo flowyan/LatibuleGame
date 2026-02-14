b3608dfaa344d80b6e524806a4c03e9ba94a03c9
Antonín <av@flwn.dev>
2026-02-14 18:05:16 +0100

    wip new model

---
+++ Latibule/Core/Rendering/Models/BillboardModel.cs
@@ -0,0 +1,11 @@
+﻿using Latibule.Core.ECS;
+
+namespace Latibule.Core.Rendering.Models;
+
+public class BillboardModel: GameObject
+{
+    public override void OnLoad()
+    {
+        base.OnLoad();
+    }
+}
