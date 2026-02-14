ca525f16114148dda84e6674735c7af96755b27c
Antonín <av@flwn.dev>
2026-02-14 02:12:53 +0100

    Corridor implemented

--- Latibule/Core/Rendering/Objects/Corridor.cs
+++ Latibule/Core/Rendering/Objects/Corridor.cs
@@ -1,5 +1,9 @@
-﻿using Latibule.Models;
+﻿using Latibule.Core.Components;
+using Latibule.Core.Data;
+using Latibule.Core.ECS;
+using Latibule.Models;
 using Microsoft.Xna.Framework;
+using Plane = Latibule.Core.Rendering.Models.Plane;
 
 namespace Latibule.Core.Rendering.Objects;
 
@@ -11,20 +15,22 @@ public class Corridor(Game game) : GameObject(game)
     {
         var localScale = new Vector3(2, 0, 2);
         var uvScale = new Vector2(1, 1);
-
-        Children =
+        BaseComponent[] components =
         [
-            new Plane(_game) { Position = new Vector3(0, 0, 0) + Position, Rotation = new Vector3(0, 0, 0), Scale = localScale, UVScale = uvScale },
-            new Plane(_game) { Position = new Vector3(2, 0, 0) + Position, Rotation = new Vector3(0, 0, 0), Scale = localScale, UVScale = uvScale },
-            new Plane(_game) { Position = new Vector3(-2, 0, 0) + Position, Rotation = new Vector3(0, 0, 0), Scale = localScale, UVScale = uvScale },
-            new Plane(_game) { Position = new Vector3(0, 2, 0) + Position, Rotation = new Vector3(0, 0, 0), Scale = localScale, UVScale = uvScale },
+            new BasicEffectComponent(_game)
+            {
+                Texture = AssetManager.GetTexture(TextureAsset.misc_tequila),
+                Color = Color.Red.ToVector3()
+            }
         ];
 
-        base.Initialize();
-    }
+        AddChildren([
+            new Plane(_game) { Position = new Vector3(0, 0, 0) + Position, Rotation = new Vector3(0, 0, 0), Scale = localScale, UVScale = uvScale, Components = components },
+            new Plane(_game) { Position = new Vector3(0, 2, -2) + Position, Rotation = new Vector3(90, 0, 0), Scale = localScale, UVScale = uvScale, Components = components },
+            new Plane(_game) { Position = new Vector3(0, 2, 2) + Position, Rotation = new Vector3(270, 0, 0), Scale = localScale, UVScale = uvScale, Components = components },
+            new Plane(_game) { Position = new Vector3(0, 4, 0) + Position, Rotation = new Vector3(180, 0, 0), Scale = localScale, UVScale = uvScale, Components = components },
+        ]);
 
-    public override void Update(GameTime gameTime)
-    {
-        base.Update(gameTime);
+        base.Initialize();
     }
 }
