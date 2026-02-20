0b6111b141dee8ac9b4db4026003f6b409db6b44
Antonín <av@flwn.dev>
2026-02-20 09:47:19 +0100

    cmd to list all lights in world

---
+++ Latibule/Commands/ListLights.cs
@@ -0,0 +1,27 @@
+﻿using Latibule.Core;
+using Latibule.Core.ECS;
+using Latibule.Core.Rendering;
+using Latibule.Models;
+
+namespace Latibule.Commands;
+
+public class ListLights : ICommand
+{
+    public string Name { get; } = "lights";
+    public List<string> Aliases { get; } = [];
+    public string Usage { get; } = "lights";
+
+    public Task Execute(string[] args)
+    {
+        var list = "";
+        foreach (var l in LatibuleGame.GameWorld.Lights)
+        {
+            if (l is null) continue;
+            list += $"- Light at ({l.Position.X},{l.Position.Y},{l.Position.Z}) (r{l.Color.X} g{l.Color.Y} b{l.Color.Z})";
+            list += "\n";
+        }
+
+        DevConsole.CommandLog(list);
+        return Task.CompletedTask;
+    }
+}
