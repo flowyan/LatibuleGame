be71b5330fe6874607e416cf5f58ecb887bf8ee8
Antonín <av@flwn.dev>
2026-02-20 09:55:31 +0100

    component for textures

---
+++ Latibule/Core/Components/TextureComponent.cs
@@ -0,0 +1,19 @@
+﻿using Latibule.Core.ECS;
+using Latibule.Core.Rendering;
+using OpenTK.Mathematics;
+
+namespace Latibule.Core.Components;
+
+public class TextureComponent(Texture texture, Vector2? uvscale = null, float uvrotate = 0f) : BaseComponent
+{
+    public Texture? Texture { get; } = texture;
+    public Vector2 UVScale { get; } = uvscale ?? Vector2.One;
+    public float UVRotation { get; } = uvrotate;
+
+    public override void Dispose()
+    {
+        base.Dispose();
+        Texture?.Dispose();
+        GC.SuppressFinalize(this);
+    }
+}
