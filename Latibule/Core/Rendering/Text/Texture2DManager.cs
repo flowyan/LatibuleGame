4d4585e74821e974d5f0edcade62a887b53b9db9
Antonín <av@flwn.dev>
2026-02-20 10:01:12 +0100

    text rendering added via fontstashsharp

---
+++ Latibule/Core/Rendering/Text/Texture2DManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Drawing;
+using FontStashSharp.Interfaces;
+
+namespace Latibule.Core.Rendering.Text;
+
+internal class Texture2DManager : ITexture2DManager
+{
+    public object CreateTexture(int width, int height) => new Texture(width, height);
+
+    public Point GetTextureSize(object texture)
+    {
+        var t = (Texture)texture;
+        return new Point(t.Width, t.Height);
+    }
+
+    public void SetTextureData(object texture, Rectangle bounds, byte[] data)
+    {
+        var t = (Texture)texture;
+        t.SetData(bounds, data);
+    }
+}
