1c7f00a83fc0aa2f8c31cae270613d0670168fd7
Antonín <av@flwn.dev>
2026-02-20 09:45:49 +0100

    changed consts to be static instead

--- Latibule/GameOptions.cs
+++ Latibule/GameOptions.cs
@@ -2,18 +2,20 @@
 
 public static class GameOptions
 {
-    public const int TargetFPS = 180;
+    public static int TargetFPS = 180;
 
-    public const bool FullScreen = false;
+    public static bool FullScreen = false;
 
     // Graphics
-    public const int FogStartDistance = 0;
-    public const int FogEndDistance = 128;
+    public static int FogStartDistance = 0;
+    public static int FogEndDistance = 128;
 
     // Mouse
-    public const float MouseSensitivity = 0.002f;
+    public static float MouseSensitivity = 0.002f;
 
     // Volume
-    public const float SoundVolume = 0.75f;
-    public const float MusicVolume = 0.5f;
+    public static float SoundVolume = 0.75f;
+    public static float MusicVolume = 0.5f;
+
+    public static int MAX_POINT_LIGHTS = 4;
 }
