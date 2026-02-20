eadda56d6cc163a122d48b2549509d38904ff561
Antonín <av@flwn.dev>
2026-02-20 09:56:25 +0100

    Added new sounds and textures

--- Latibule/Core/Data/SoundAsset.cs
+++ Latibule/Core/Data/SoundAsset.cs
@@ -3,5 +3,7 @@
 public enum SoundAsset
 {
     missing,
-    tada
+    tada,
+    sleepphonk,
+    scarletfire,
 }
--- Latibule/Core/Data/TextureAsset.cs
+++ Latibule/Core/Data/TextureAsset.cs
@@ -2,7 +2,14 @@
 
 public enum TextureAsset
 {
+    // ROOT //
     missing,
+
+    // MATERIAL //
     material_stone,
+    material_tiles,
+    material_concrete,
+
+    // MISC //
     misc_tequila
 }
