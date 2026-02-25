090b4254e1443660ea2b8fd8722fdaf5659948ae
Antonín <av@flwn.dev>
2026-02-25 16:46:15 +0100

    transparency detection and texture options

--- Latibule/Core/Rendering/Texture.cs
+++ Latibule/Core/Rendering/Texture.cs
@@ -1,9 +1,20 @@
-﻿using System.Drawing;
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Drawing;
 using Latibule.Core.Rendering.Helpers;
 using OpenTK.Graphics.OpenGL4;
 using StbImageSharp;
 
+
 namespace Latibule.Core.Rendering;
+
+[SuppressMessage("ReSharper", "FieldCanBeMadeReadOnly.Global")]
+[SuppressMessage("ReSharper", "ConvertToConstant.Global")]
+public class TextureOptions
+{
+    public TextureWrapMode WrapMode = TextureWrapMode.Repeat;
+    public TextureMinFilter MinFilter = TextureMinFilter.Nearest;
+    public TextureMagFilter MagFilter = TextureMagFilter.Nearest;
+}
 
 public class Texture : IDisposable
 {
@@ -11,9 +22,11 @@ public class Texture : IDisposable
 
     public readonly int Width;
     public readonly int Height;
+    public bool HasTransparency { get; private set; }
 
-    public Texture(int width, int height)
+    public Texture(int width, int height, TextureOptions? options = null)
     {
+        options ??= new TextureOptions();
         Width = width;
         Height = height;
 
@@ -21,14 +34,16 @@ public class Texture : IDisposable
         GLUtility.CheckError();
         Bind();
 
+        GL.PixelStore(PixelStoreParameter.UnpackAlignment, 1);
         GL.TexImage2D(TextureTarget.Texture2D, 0, PixelInternalFormat.Rgba8, width, height, 0, PixelFormat.Rgba, PixelType.UnsignedByte, IntPtr.Zero);
         GLUtility.CheckError();
 
-        SetParameters();
+        SetParameters(options);
     }
 
-    public Texture(string path)
+    public Texture(string path, TextureOptions? options = null)
     {
+        options ??= new TextureOptions();
         _handle = GL.GenTexture();
         GLUtility.CheckError();
         Bind();
@@ -38,27 +53,28 @@ public class Texture : IDisposable
         using (var stream = File.OpenRead(path))
         {
             var image = ImageResult.FromStream(stream, ColorComponents.RedGreenBlueAlpha);
+            HasTransparency = DetectTransparency(image.Data);
             Width = image.Width;
             Height = image.Height;
-            GL.TexImage2D(TextureTarget.Texture2D, 0, PixelInternalFormat.Rgba, image.Width, image.Height, 0, PixelFormat.Rgba, PixelType.UnsignedByte, image.Data);
+            GL.TexImage2D(TextureTarget.Texture2D, 0, PixelInternalFormat.Rgba8, image.Width, image.Height, 0, PixelFormat.Rgba, PixelType.UnsignedByte, image.Data);
             GLUtility.CheckError();
         }
 
-        SetParameters();
+        SetParameters(options);
     }
 
-    public void SetParameters()
+    public void SetParameters(TextureOptions options)
     {
-        GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureWrapS, (int)TextureWrapMode.Repeat);
+        GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureWrapS, (int)options.WrapMode);
         GLUtility.CheckError();
 
-        GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureWrapT, (int)TextureWrapMode.Repeat);
+        GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureWrapT, (int)options.WrapMode);
         GLUtility.CheckError();
 
-        GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMinFilter, (int)TextureMinFilter.Nearest);
+        GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMinFilter, (int)options.MinFilter);
         GLUtility.CheckError();
 
-        GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMagFilter, (int)TextureMagFilter.Nearest);
+        GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMagFilter, (int)options.MagFilter);
         GLUtility.CheckError();
 
         GL.GenerateMipmap(GenerateMipmapTarget.Texture2D);
@@ -99,5 +115,14 @@ public class Texture : IDisposable
             );
             GLUtility.CheckError();
         }
+    }
+
+    private static bool DetectTransparency(byte[] rgba)
+    {
+        // RGBA = 4 bytes per pixel, alpha at index 3
+        for (int i = 3; i < rgba.Length; i += 4)
+            if (rgba[i] != 255)
+                return true;
+        return false;
     }
 }
