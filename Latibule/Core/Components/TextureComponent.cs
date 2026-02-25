221e6a974d482e6c12248a8d3d2116d11d823485
Antonín <av@flwn.dev>
2026-02-25 16:52:58 +0100

    texture component improved

--- Latibule/Core/Components/TextureComponent.cs
+++ Latibule/Core/Components/TextureComponent.cs
@@ -4,16 +4,40 @@ using OpenTK.Mathematics;
 
 namespace Latibule.Core.Components;
 
-public class TextureComponent(Texture texture, Vector2? uvscale = null, float uvrotate = 0f) : BaseComponent
+public class TextureComponent : BaseComponent
 {
-    public Texture? Texture { get; } = texture;
-    public Vector2 UVScale { get; } = uvscale ?? Vector2.One;
-    public float UVRotation { get; } = uvrotate;
+    public Texture[] Textures { get; set; }
+    public Vector2 UVScale { get; }
+    public float UVRotation { get; }
+
+    public TextureComponent(Texture texture, Vector2? uvscale = null, float uvrotate = 0f)
+        : this([texture], uvscale, uvrotate)
+    {
+    }
+
+    public TextureComponent(Vector2? uvscale = null, float uvrotate = 0f, params Texture[] textures)
+        : this(textures, uvscale, uvrotate)
+    {
+    }
+
+    public TextureComponent(Texture[] textures, Vector2? uvscale = null, float uvrotate = 0f)
+    {
+        if (textures is null || textures.Length == 0)
+            throw new ArgumentException("At least one texture is required.", nameof(textures));
+
+        if (Array.Exists(textures, t => t is null))
+            throw new ArgumentException("Textures cannot contain null entries.", nameof(textures));
+
+        Textures = textures;
+        UVScale = uvscale ?? Vector2.One;
+        UVRotation = uvrotate;
+    }
 
     public override void Dispose()
     {
         base.Dispose();
-        Texture?.Dispose();
+        foreach (var tex in Textures)
+            tex.Dispose();
         GC.SuppressFinalize(this);
     }
 }
