2e0738f8308cee95b5493758f1c4c38f81223f3a
Antonín <av@flwn.dev>
2026-02-25 16:25:07 +0100

    vector converter for vector2 3 and 4

--- Latibule/Utilities/VectorConverter.cs
+++ Latibule/Utilities/VectorConverter.cs
@@ -1,7 +1,17 @@
 ﻿﻿namespace Latibule.Utilities;
 
-public static class Vector3Converter
+public static class VectorConverter
 {
+    public static OpenTK.Mathematics.Vector4 ToOpenTK(this System.Numerics.Vector4 vector)
+    {
+        return new OpenTK.Mathematics.Vector4(vector.X, vector.Y, vector.Z, vector.W);
+    }
+
+    public static System.Numerics.Vector4 ToNumerics(this OpenTK.Mathematics.Vector4 vector)
+    {
+        return new System.Numerics.Vector4(vector.X, vector.Y, vector.Z, vector.W);
+    }
+
     public static OpenTK.Mathematics.Vector3 ToOpenTK(this System.Numerics.Vector3 vector)
     {
         return new OpenTK.Mathematics.Vector3(vector.X, vector.Y, vector.Z);
