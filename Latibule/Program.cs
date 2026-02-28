85f8d6219d9535d9803bc696243e9944138b91dd
Antonín <av@flwn.dev>
2026-02-28 03:47:26 +0100

    bigger default window size, upped opengl to 4.6

--- Latibule/Program.cs
+++ Latibule/Program.cs
@@ -12,13 +12,14 @@ public static class Program
 {
     private static readonly NativeWindowSettings _windowSettings = new()
     {
-        ClientSize = new Vector2i(1280, 720),
+        ClientSize = new Vector2i(1920, 1080),
         Title = $"{Metadata.GAME_NAME} {Metadata.GAME_VERSION}",
         Icon = LoadIcon("Assets/icon.jpg"),
-        APIVersion = new Version(4, 1),
+        APIVersion = new Version(4, 6),
         Flags = ContextFlags.ForwardCompatible | ContextFlags.Debug,
         Profile = ContextProfile.Core,
         Vsync = VSyncMode.Off,
+        NumberOfSamples = 4,
     };
 
     private static WindowIcon LoadIcon(string path)
