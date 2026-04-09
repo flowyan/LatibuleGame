d47f146f9d0c8d9684c186d30a255ffac3f4cc1f
Antonín <av@flwn.dev>
2026-04-09 04:01:50 +0200

    new map for testing joltphysics

---
+++ Latibule/Maps/PhysicsTestMap.cs
@@ -0,0 +1,64 @@
+﻿using Engine.Components;
+using Engine.Core.ECS;
+using Engine.Core.Types;
+using Engine.Data;
+using Engine.Data.Sound;
+using Engine.Data.Textures;
+using Engine.Physics;
+using Engine.Rendering;
+using FontStashSharp;
+using Latibule.Components;
+using Latibule.Data.Sound;
+using Latibule.Entities;
+using Latibule.Objects;
+using OpenTK.Mathematics;
+using Textures = Latibule.Data.Texture.Textures;
+
+namespace Latibule.Maps;
+
+public static class PhysicsTestMap
+{
+    public static GameMap Create()
+    {
+        var map = new GameMap();
+
+        // floor
+        // map.AddObject(new PlaneObject { Transform = { Position = new Vector3(0, 0, 0), Scale = new Vector3(10, 0, 10) } }
+        //     .WithComponent(new TextureComponent(Asseteer.GetTexture(EngineTextures.Dev.dev_measuregeneric01b), new Vector2(10, 10))));
+
+        // // Walls
+        // map.AddObject(new PlaneObject { Transform = { Position = new Vector3(10, 2, 0), Scale = new Vector3(2, 0, 10), Rotation = new Vector3(0, 0, 90) } }
+        //     .WithComponent(new TextureComponent(Asseteer.GetTexture(EngineTextures.Dev.dev_measuregeneric01), new Vector2(2, 10), 90f)));
+        // map.AddObject(new PlaneObject { Transform = { Position = new Vector3(-10, 2, 0), Scale = new Vector3(2, 0, 10), Rotation = new Vector3(0, 0, 270) } }
+        //     .WithComponent(new TextureComponent(Asseteer.GetTexture(EngineTextures.Dev.dev_measuregeneric01), new Vector2(2, 10), -90f)));
+        // map.AddObject(new PlaneObject { Transform = { Position = new Vector3(0, 2, 10), Scale = new Vector3(2, 0, 10), Rotation = new Vector3(-90, 0, 90) } }
+        //     .WithComponent(new TextureComponent(Asseteer.GetTexture(EngineTextures.Dev.dev_measuregeneric01), new Vector2(2, 10), 90f)));
+        // map.AddObject(new PlaneObject { Transform = { Position = new Vector3(0, 2, -10), Scale = new Vector3(2, 0, 10), Rotation = new Vector3(90, 0, -90) } }
+        //     .WithComponent(new TextureComponent(Asseteer.GetTexture(EngineTextures.Dev.dev_measuregeneric01), new Vector2(2, 10), -90f)));
+
+        // Text ui object
+        map.AddObject(new GameObject { Transform = { Position = new(0, 3, 5) } }
+            .WithComponents(new WorldTextRendererComponent(new TextRendererOptions
+            {
+                text = "physics test map",
+                fontSize = 32,
+                color = FSColor.White,
+                fontSystemEffect = FontSystemEffect.Stroked,
+                effectAmount = 4,
+                billboard = BillboardEnum.YLocked
+            })));
+
+        // Lights
+        map.AddPointLight(new PointLight { Position = new Vector3(0, 2, 0), Color = new Vector3(1f, 1f, 1f) });
+
+        // map.AddObject(new Maxwell(false) { Transform = { Position = new Vector3(0, 0.5f, 8.5f) } });
+
+        Asseteer.PlaySound(EngineSounds.Dev.tada, 0.5f);
+
+        LatibuleGame.Player = new Player { Transform = { Position = new Vector3(0, 0.1f, 0) } };
+        LatibuleGame.Player.IsNoclip = true;
+        LatibuleGame.Player.WithComponents(new DebugInfoOverlay(), new DebugBoundingBoxOutlineOverlay(), new DebugPointLightRendererOverlay());
+        map.AddObject(LatibuleGame.Player); // Player should always be added last
+        return map;
+    }
+}
--- Latibule/Maps/TestingMap.cs
+++ Latibule/Maps/TestingMap.cs
@@ -2,23 +2,24 @@
 using Engine.Core.ECS;
 using Engine.Core.Types;
 using Engine.Data;
+using Engine.Data.Shaders;
 using Engine.Rendering;
 using Engine.Rendering.Shapes;
 using FontStashSharp;
 using Latibule.Components;
-using Latibule.Core.Data;
+using Latibule.Data.Texture;
 using Latibule.Entities;
 using Latibule.Items;
 using Latibule.Objects;
 using OpenTK.Mathematics;
 
-namespace Latibule;
+namespace Latibule.Maps;
 
 public static class TestingMap
 {
     public static GameMap Create()
     {
-        var meshShader = Asseteer.GetShader(InternalShaderAsset.mesh_shader);
+        var meshShader = Asseteer.GetShader(EngineShaders.DefaultShader);
         var world = new GameMap();
 
         world.AddObject(new GameObject
@@ -29,18 +30,18 @@ public static class TestingMap
                 Scale = new Vector3(2, 2, 2),
                 Rotation = new Vector3(0, 270, 0)
             }
-        }.WithComponents(new ShaderComponent(meshShader), new ShapeRendererComponent(new IsoSphere(8)), new BoundingBoxComponent(), new TextureComponent(Asseteer.GetTexture(TextureAsset.misc_tequila))));
+        }.WithComponents(new ShaderComponent(meshShader), new ShapeRendererComponent(new IsoSphere(8)), new BoundingBoxComponent(), new TextureComponent(Asseteer.GetTexture(Textures.Misc.tequila))));
 
         world.AddObject(new GameObject
         {
             Transform = { Position = new Vector3(0, 1, -7.5f), Scale = new Vector3(0.5f) }
-        }.WithComponents(new ShaderComponent(meshShader), new ShapeRendererComponent(new Cube()), new BoundingBoxComponent(), new TextureComponent(Asseteer.GetTexture(TextureAsset.misc_speaker))));
+        }.WithComponents(new ShaderComponent(meshShader), new ShapeRendererComponent(new Cube()), new BoundingBoxComponent(), new TextureComponent(Asseteer.GetTexture(Textures.Misc.speaker))));
 
         world.AddPointLight(new PointLight { Position = new Vector3(5, 1.5f, 0), Color = new Vector3(0f, 1f, 0f), Intensity = 0.5f });
         world.AddObject(new GameObject
         {
             Transform = { Position = new Vector3(5, 0.5f, 0), Scale = new Vector3(0.5f) }
-        }.WithComponents(new ShaderComponent(meshShader), new ShapeRendererComponent(new Cube()), new BoundingBoxComponent(), new TextureComponent(Asseteer.GetTexture(TextureAsset.heyo_greensquare))));
+        }.WithComponents(new ShaderComponent(meshShader), new ShapeRendererComponent(new Cube()), new BoundingBoxComponent(), new TextureComponent(Asseteer.GetTexture(Textures.Misc.greensquare))));
 
 
         // floor
@@ -48,19 +49,19 @@ public static class TestingMap
             {
                 Transform = { Position = new Vector3(0, 0, 0), Scale = new Vector3(10, 0, 10) }
             }
-            .WithComponent(new TextureComponent(Asseteer.GetTexture(TextureAsset.material_tiles), new Vector2(10, 10))));
+            .WithComponent(new TextureComponent(Asseteer.GetTexture(Textures.Material.tiles), new Vector2(10, 10))));
 
         // Walls
         world.AddObject(new PlaneObject { Transform = { Position = new Vector3(10, 2, 6), Scale = new Vector3(2, 0, 4), Rotation = new Vector3(0, 0, 90) } }
-            .WithComponent(new TextureComponent(Asseteer.GetTexture(TextureAsset.material_concrete), new Vector2(2, 4), 90f)));
+            .WithComponent(new TextureComponent(Asseteer.GetTexture(Textures.Material.concrete), new Vector2(2, 4), 90f)));
         world.AddObject(new PlaneObject { Transform = { Position = new Vector3(10, 2, -6), Scale = new Vector3(2, 0, 4), Rotation = new Vector3(0, 0, 90) } }
-            .WithComponent(new TextureComponent(Asseteer.GetTexture(TextureAsset.material_concrete), new Vector2(2, 4), 90f)));
+            .WithComponent(new TextureComponent(Asseteer.GetTexture(Textures.Material.concrete), new Vector2(2, 4), 90f)));
         world.AddObject(new PlaneObject { Transform = { Position = new Vector3(-10, 2, 0), Scale = new Vector3(2, 0, 10), Rotation = new Vector3(0, 0, 270) } }
-            .WithComponent(new TextureComponent(Asseteer.GetTexture(TextureAsset.material_concrete), new Vector2(2, 10), -90f)));
+            .WithComponent(new TextureComponent(Asseteer.GetTexture(Textures.Material.concrete), new Vector2(2, 10), -90f)));
         world.AddObject(new PlaneObject { Transform = { Position = new Vector3(0, 2, 10), Scale = new Vector3(2, 0, 10), Rotation = new Vector3(-90, 0, 90) } }
-            .WithComponent(new TextureComponent(Asseteer.GetTexture(TextureAsset.material_concrete), new Vector2(2, 10), 90f)));
+            .WithComponent(new TextureComponent(Asseteer.GetTexture(Textures.Material.concrete), new Vector2(2, 10), 90f)));
         world.AddObject(new PlaneObject { Transform = { Position = new Vector3(0, 2, -10), Scale = new Vector3(2, 0, 10), Rotation = new Vector3(90, 0, -90) } }
-            .WithComponent(new TextureComponent(Asseteer.GetTexture(TextureAsset.material_concrete), new Vector2(2, 10), -90f)));
+            .WithComponent(new TextureComponent(Asseteer.GetTexture(Textures.Material.concrete), new Vector2(2, 10), -90f)));
 
         // Corridor
         world.AddObject(new Corridor { Transform = { Position = new Vector3(12, 0, 0) } });
@@ -93,7 +94,7 @@ public static class TestingMap
 
         // world.AddObject(new GameObject { Transform = { Position = new Vector3(50, 0, 0), Scale = new Vector3(0.01f) } }.WithComponents(
         //     new ShaderComponent(meshShader),
-        //     new TextureComponent(Asseteer.GetTexture(TextureAsset.material_concrete)),
+        //     new TextureComponent(Asseteer.GetTexture(Textures.Material.concrete)),
         //     new ModelRendererComponent(Asseteer.GetModel(ModelAsset.sponza))
         // ));
 
