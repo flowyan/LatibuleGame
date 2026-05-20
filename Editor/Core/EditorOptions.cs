56f65584a01f552d151c3c8b4945ea9a0ce792ad
Antonin <av@flwn.dev>
2026-05-21 00:33:21 +0200

    feat: update EditorOptions for grid settings and add map rendering options

--- Editor/Core/EditorOptions.cs
+++ Editor/Core/EditorOptions.cs
@@ -3,8 +3,12 @@
 public static class EditorOptions
 {
     public static int TargetFPS = 60;
-    public static float GridGap = 4f;
+    public static float GridGap = 0f;
     public static float LeftPanelRatio = 0.2f;
     public static float RightPanelRatio = 0.25f;
-    public static float TopSectionRatio = 0.75f;
+    public static float TopSectionRatio = 0.65f;
+    
+    // Used inside MapSettingsWindow.cs
+    public static bool RenderBoundingBoxes = true;
+    public static bool UpdateMap = false;
 }
