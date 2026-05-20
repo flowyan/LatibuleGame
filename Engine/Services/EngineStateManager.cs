7d9722de5e157c80a9c332b54e46ca1f101f78ce
Antonin <av@flwn.dev>
2026-05-21 00:30:06 +0200

    fix: update Input initialization and update methods to include mouse state

--- Engine/Services/EngineStateManager.cs
+++ Engine/Services/EngineStateManager.cs
@@ -8,13 +8,13 @@ public static class EngineStateManager
     public static void Initialize(GameWindow gameWindow)
     {
         EngineStates.Initialize(gameWindow);
-        Input.Initialize(gameWindow.KeyboardState);
+        Input.Initialize(gameWindow.KeyboardState, gameWindow.MouseState);
         EngineStates.GameWindow =  gameWindow;
     }
 
     public static void Update(GameWindow gameWindow)
     {
         EngineStates.GameWindow = gameWindow;
-        Input.Update(gameWindow.KeyboardState);
+        Input.Update(gameWindow.KeyboardState, gameWindow.MouseState);
     }
 }
