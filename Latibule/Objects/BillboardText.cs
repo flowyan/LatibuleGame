be188ce532b09d8f6d95f3c20b23de8d9c19a62e
Antonin <av@flwn.dev>
2026-05-21 00:22:08 +0200

    helper to create billboarded text easily in the world

---
+++ Latibule/Objects/BillboardText.cs
@@ -0,0 +1,27 @@
+﻿using Engine.Components;
+using Engine.Core.ECS;
+using Engine.Core.Types;
+using FontStashSharp;
+
+namespace Latibule.Objects;
+
+/// <summary>
+/// A billboard text object that always faces the camera, used for rendering text in the world.
+/// </summary>
+public class BillboardText(string text, BillboardEnum billboard = BillboardEnum.YLocked, FSColor? color = null, float fontSize = 32) : GameObject, IPrefab
+{
+    public override void OnLoad()
+    {
+        base.OnLoad();
+
+        WithComponents(new WorldTextRendererComponent(new TextRendererOptions
+        {
+            text = text,
+            fontSize = fontSize,
+            color = color ?? FSColor.White,
+            fontSystemEffect = FontSystemEffect.Stroked,
+            effectAmount = 4,
+            billboard = billboard
+        }));
+    }
+}
