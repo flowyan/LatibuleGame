dc3e646a3e8c4fd654a4445f5a0c02beee203194
Antonin <av@flwn.dev>
2026-05-21 00:22:53 +0200

    updated maxwell to use mesh shape for physics

--- Latibule/Objects/Maxwell.cs
+++ Latibule/Objects/Maxwell.cs
@@ -1,8 +1,12 @@
-﻿using Engine.Components;
+﻿using Engine;
+using Engine.Components;
 using Engine.Core.ECS;
+using Engine.Core.Types;
 using Engine.Data;
 using Engine.Data.Shaders;
-using Latibule.Data;
+using Engine.Physics;
+using Engine.Utilities;
+using JoltPhysicsSharp;
 using Latibule.Data.Model;
 using Latibule.Data.Texture;
 using OpenTK.Mathematics;
@@ -10,24 +14,31 @@ using OpenTK.Windowing.Common;
 
 namespace Latibule.Objects;
 
-public class Maxwell(bool spin = false) : GameObject
+public class Maxwell(bool spin = false) : GameObject, IPrefab
 {
     private float _startY;
     private float _t;
-    private BoundingBoxComponent _collision;
 
     public override void OnLoad()
     {
         base.OnLoad();
 
-        _collision = new BoundingBoxComponent(scale: new Vector3(0.7f, 0.5f, 0.5f));
+        var model = Asseteer.GetModel(Models.Misc.maxwell);
 
         WithComponents([
             new ShaderComponent(Asseteer.GetShader(EngineShaders.DefaultShader)),
             new TextureComponent(Asseteer.GetTextures([Textures.Models.Maxwell.maxwell, Textures.Models.Maxwell.whiskers])),
-            new ModelRendererComponent(Asseteer.GetModel(Models.Misc.maxwell)),
-            _collision
+            new ModelRendererComponent(model),
         ]);
+
+        using BodyCreationSettings creationSettings = new(
+            new StaticCompoundShape(PhysSettings.StaticCompoundShape(model, [1])),
+            Transform.Position.ToNumerics(),
+            Transform.Rotation.ToQuaternion(),
+            MotionType.Kinematic,
+            JoltPhysics.Layers.Moving
+        );
+        PhysicsBodyID = LatibuleEngine.Physics.BodyInterface.CreateAndAddBody(creationSettings, Activation.DontActivate);
 
         _startY = Transform.Position.Y;
     }
@@ -46,9 +57,14 @@ public class Maxwell(bool spin = false) : GameObject
         var pos = Transform.Position;
         pos.Y = _startY + ((MathF.Sin(_t * speed) + 1f) * 0.5f) * maxHeight;
         Transform.Position = pos;
-        _collision.Position = pos;
-
         Transform.Rotation += new Vector3(0f, spinage, 0f);
-        _collision.Rotation = Transform.Rotation;
+
+        if (PhysicsBodyID != null)
+            LatibuleEngine.Physics.BodyInterface.SetPositionAndRotation(
+                PhysicsBodyID.Value,
+                Transform.Position.ToNumerics(),
+                Transform.Rotation.ToQuaternion(),
+                Activation.DontActivate
+            );
     }
 }
