b7a615334097041e9334fb6e4576175f78f9f6e1
Antonin <av@flwn.dev>
2026-05-21 00:32:19 +0200

    feat: add ProjectWindow for displaying loaded prefabs in the editor

---
+++ Editor/Windows/ProjectWindow.cs
@@ -0,0 +1,43 @@
+﻿using System.Reflection;
+using Editor.Core.Types;
+using Engine.Core;
+using ImGuiNET;
+using OpenTK.Windowing.Common;
+
+namespace Editor.Windows;
+
+public sealed class ProjectWindow : IEditorWindow, IDisposable
+{
+    public string Title => "Project";
+    public EditorWindowSlot Slot => EditorWindowSlot.BottomFullWidth;
+
+    private List<Type> prefabs = [];
+
+    public ProjectWindow()
+    {
+        foreach (var gameObject in Assembly.GetExecutingAssembly().GetTypes()
+                     .ToList())
+        {
+            Logger.LogWarning($"Loaded prefab: {gameObject.Name}");
+            prefabs.Add(gameObject);
+        }
+    }
+
+    public void Render(FrameEventArgs e)
+    {
+        ImGui.BeginChild("##editor-project-prefabs", new System.Numerics.Vector2(0, -ImGui.GetFrameHeightWithSpacing()));
+        foreach (var prefab in prefabs)
+        {
+            ImGui.Button(prefab.Name);
+        }
+        ImGui.EndChild();
+    }
+
+    public void Update(FrameEventArgs e)
+    {
+    }
+
+    public void Dispose()
+    {
+    }
+}
