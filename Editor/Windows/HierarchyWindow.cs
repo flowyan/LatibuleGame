46a34d862773a76a65aa1ce0c589ca66a4336e1f
Antonin <av@flwn.dev>
2026-05-21 00:32:36 +0200

    feat: enhance HierarchyWindow with object listing and selection buttons

--- Editor/Windows/HierarchyWindow.cs
+++ Editor/Windows/HierarchyWindow.cs
@@ -10,15 +10,34 @@ public sealed class HierarchyWindow : IEditorWindow
     public string Title => "Hierarchy";
     public EditorWindowSlot Slot => EditorWindowSlot.TopLeft;
 
-    public void Render(FrameEventArgs e)
+    public unsafe void Render(FrameEventArgs e)
     {
         ImGui.Text("Scene graph");
         ImGui.Separator();
 
-        foreach (var gameObject in LatibuleEngine.Map.Objects)
+        var counter = 0;
+        foreach (var go in LatibuleEngine.Map.Objects)
         {
-            ImGui.Text(gameObject.ToString());
+            counter++;
+            if (go.Parent != null) continue;
+            if (ImGui.SmallButton($"[{counter.ToString().PadLeft(3, '0')}] {go.EditorName()}")) MalletEditor.SelectObject(go);
+            if (go.Children.Any())
+            {
+                ImGui.Indent();
+                ImGui.PushStyleColor(ImGuiCol.Button, new System.Numerics.Vector4(0.5f, 0.5f, 0.5f, 1f));
+                foreach (var child in go.Children)
+                {
+                    if (ImGui.SmallButton($"└ {child.EditorName()}")) MalletEditor.SelectObject(go);
+                }
+
+                ImGui.PopStyleColor();
+
+                ImGui.Unindent();
+            }
         }
     }
-}
 
+    public void Update(FrameEventArgs e)
+    {
+    }
+}
