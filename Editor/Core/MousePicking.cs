83bada1a65f403ea06081db8fa3aeabf7add8e3b
Antonin <av@flwn.dev>
2026-05-21 00:33:27 +0200

    feat: implement raycasting for physics body intersection in MousePicking

--- Editor/Core/MousePicking.cs
+++ Editor/Core/MousePicking.cs
@@ -1,5 +1,8 @@
-﻿using Engine.Physics;
+﻿using Engine;
+using Engine.Core.ECS;
+using Engine.Utilities;
 using OpenTK.Mathematics;
+using Ray = Engine.Physics.Ray;
 
 namespace Editor.Core;
 
@@ -11,84 +14,71 @@ public static class MousePicking
         Matrix4 view,
         Matrix4 projection)
     {
-        // Convert from pixel coords to NDC (-1 to 1).
-        // Y is flipped because screen coords usually go top->down,
-        // while NDC goes bottom->top.
-        float x = (2f * mousePositionInViewport.X) / viewportSize.X - 1f;
-        float y = 1f - (2f * mousePositionInViewport.Y) / viewportSize.Y;
+        // Convert viewport pixel coords to OpenGL NDC coordinates.
+        var x = (2f * mousePositionInViewport.X) / viewportSize.X - 1f;
+        var y = 1f - (2f * mousePositionInViewport.Y) / viewportSize.Y;
 
-        Vector4 rayClipNear = new Vector4(x, y, -1f, 1f);
-        Vector4 rayClipFar = new Vector4(x, y, 1f, 1f);
+        var rayClipNear = new Vector4(x, y, -1f, 1f);
+        var rayClipFar = new Vector4(x, y, 1f, 1f);
 
-        Matrix4 invViewProj = Matrix4.Invert(view * projection);
+        var invViewProj = Matrix4.Invert(view * projection);
 
-        Vector4 nearWorld4 = Vector4.TransformRow(rayClipNear, invViewProj);
-        Vector4 farWorld4 = Vector4.TransformRow(rayClipFar, invViewProj);
+        var nearWorld4 = Vector4.TransformRow(rayClipNear, invViewProj);
+        var farWorld4 = Vector4.TransformRow(rayClipFar, invViewProj);
 
         nearWorld4 /= nearWorld4.W;
         farWorld4 /= farWorld4.W;
 
-        Vector3 nearWorld = nearWorld4.Xyz;
-        Vector3 farWorld = farWorld4.Xyz;
+        var nearWorld = nearWorld4.Xyz;
+        var farWorld = farWorld4.Xyz;
 
-        Vector3 direction = (farWorld - nearWorld).Normalized();
-
+        var direction = (farWorld - nearWorld).Normalized();
         return new Ray(nearWorld, direction);
     }
 
-    public static bool IntersectPlane(
-        Ray ray,
-        Vector3 planePoint,
-        Vector3 planeNormal,
-        out Vector3 hitPoint)
+    public static bool TryIntersectPhysicsBody(
+        Ray rayInput,
+        IEnumerable<GameObject> objects,
+        out GameObject? hitObject,
+        out Vector3 hitPoint
+    )
     {
+        hitObject = null;
         hitPoint = Vector3.Zero;
 
-        float denom = Vector3.Dot(ray.Direction, planeNormal);
-        if (MathF.Abs(denom) < 0.0001f)
-            return false;
+        var physicsSystem = LatibuleEngine.Physics.PhysicsSystem;
+        var directionWithLength = Vector3.Normalize(rayInput.Direction) * 1000f;
+        var ray = new JoltPhysicsSharp.Ray(rayInput.Position.ToNumerics(), directionWithLength.ToNumerics());
 
-        float t = Vector3.Dot(planePoint - ray.Position, planeNormal) / denom;
-        if (t < 0f)
-            return false;
+        var hasHit = physicsSystem.NarrowPhaseQuery.CastRay(
+            ray,
+            out var hitResult,
+            null,
+            null,
+            null
+        );
 
-        hitPoint = ray.Position + ray.Direction * t;
-        return true;
-    }
+        if (hasHit)
+        {
+            var hitFraction = hitResult.Fraction;
+            var intersectionPoint = ray.GetPointOnRay(hitFraction);
 
-    public static bool IntersectAabb(
-        Ray ray,
-        Vector3 min,
-        Vector3 max,
-        out float distance)
-    {
-        distance = 0f;
+            hitPoint = intersectionPoint.ToOpenTK();
+        }
 
-        float tMin = (min.X - ray.Position.X) / ray.Direction.X;
-        float tMax = (max.X - ray.Position.X) / ray.Direction.X;
-        if (tMin > tMax) (tMin, tMax) = (tMax, tMin);
+        // this is where we get what object has the bodyid we hit
+        // (there should be a key value dictionary in the future with bodyid as key and gameobject as value to speed this up)
+        foreach (var obj in objects)
+        {
+            var bodyId = obj.PhysicsBodyID;
+            if (bodyId is null)
+                continue;
 
-        float tyMin = (min.Y - ray.Position.Y) / ray.Direction.Y;
-        float tyMax = (max.Y - ray.Position.Y) / ray.Direction.Y;
-        if (tyMin > tyMax) (tyMin, tyMax) = (tyMax, tyMin);
+            if (bodyId.Value != hitResult.BodyID) continue;
+            hitObject = obj;
+            return true;
+        }
 
-        if (tMin > tyMax || tyMin > tMax)
-            return false;
-
-        if (tyMin > tMin) tMin = tyMin;
-        if (tyMax < tMax) tMax = tyMax;
-
-        float tzMin = (min.Z - ray.Position.Z) / ray.Direction.Z;
-        float tzMax = (max.Z - ray.Position.Z) / ray.Direction.Z;
-        if (tzMin > tzMax) (tzMin, tzMax) = (tzMax, tzMin);
-
-        if (tMin > tzMax || tzMin > tMax)
-            return false;
-
-        if (tzMin > tMin) tMin = tzMin;
-        if (tzMax < tMax) tMax = tzMax;
-
-        distance = tMin < 0f ? tMax : tMin;
-        return distance >= 0f;
+        return hitObject is not null;
     }
 }
