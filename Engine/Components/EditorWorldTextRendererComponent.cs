88df5b1b29f303a24baf466495ca5f1aa9b9fac5
Antonin <av@flwn.dev>
2026-05-21 00:25:38 +0200

    helper for adding inworld text that renders over all

---
+++ Engine/Components/EditorWorldTextRendererComponent.cs
@@ -0,0 +1,33 @@
+﻿using Engine.Core.ECS;
+using Engine.Core.Types;
+using Engine.Rendering;
+using Engine.Rendering.Renderer;
+using OpenTK.Graphics.OpenGL4;
+using OpenTK.Windowing.Common;
+
+namespace Engine.Components;
+
+public class EditorWorldTextRendererComponent : BaseComponent
+{
+    private WorldTextRenderer? _renderer;
+    private readonly TextRendererOptions _options;
+
+    // static text
+    public EditorWorldTextRendererComponent(TextRendererOptions options)
+    {
+        _options = options;
+        RenderLayer = RenderLayer.UI;
+    }
+
+    public override void OnLoad(GameObject gameObject)
+    {
+        base.OnLoad(gameObject);
+        _renderer = new WorldTextRenderer(_options);
+    }
+
+    public override void OnRenderFrame(FrameEventArgs args)
+    {
+        base.OnRenderFrame(args);
+        _renderer?.Render(Parent.Transform, _options.text, true);
+    }
+}
