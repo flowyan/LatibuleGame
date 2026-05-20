c5ef7cfa043efe293592398ad5bbe3e3c465e9d0
Antonin <av@flwn.dev>
2026-05-21 00:32:44 +0200

    feat: add BottomTabsWindow for managing Project and Console tabs

---
+++ Editor/Windows/BottomTabsWindow.cs
@@ -0,0 +1,45 @@
+﻿using Editor.Core.Types;
+using ImGuiNET;
+using OpenTK.Windowing.Common;
+
+namespace Editor.Windows;
+
+public sealed class BottomTabsWindow : IEditorWindow, IDisposable
+{
+    public string? Title => null;
+    public EditorWindowSlot Slot => EditorWindowSlot.BottomFullWidth;
+    
+    private readonly ProjectWindow projectWindow = new();
+    private readonly ConsoleWindow consoleWindow = new();
+
+    public void Render(FrameEventArgs e)
+    {
+        ImGui.BeginTabBar("Tabs");
+        if (ImGui.BeginTabItem("Project"))
+        {
+            projectWindow.Render(e);
+            ImGui.EndTabItem();
+        }
+
+        if (ImGui.BeginTabItem("Console"))
+        {
+            consoleWindow.Render(e);
+            ImGui.EndTabItem();
+        }
+            
+        
+        ImGui.EndTabBar();
+    }
+
+    public void Update(FrameEventArgs e)
+    {
+        projectWindow.Update(e);
+        consoleWindow.Update(e);
+    }
+
+    public void Dispose()
+    {
+        projectWindow.Dispose();
+        consoleWindow.Dispose();
+    }
+}
